0184fc7664b85207cc39e3560562cec4044af4db
PSkoczenski <pskoczenski@psko-2.local>
2020-01-23 09:18:16 -0500

    first steops of grommet integration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React from "react";
-import "./styles/App.css";
+import { Box, Grommet } from 'grommet';
 import Nav from "./components/main-nav";
 import Search from "./components/search";
 import AlbumSearch from "./components/album-search";
+// import gromBox from "./components/grom-comps/gromBox"
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
+const gromBox = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Grommet theme={theme}>
+      <gromBox>
         <h1>Vinyl Bin</h1>
-        {/* <Nav /> */}
-      </header>
+      </gromBox>
       <div>
         <Search />
         {/* <AlbumSearch /> */}
       </div>
-    </div>
+      
+    </Grommet>
   );
 }
 
---
+++ src/components/grom-comps/gromBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Grommet } from 'grommet';
+
+const gromBox = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+export default gromBox;
